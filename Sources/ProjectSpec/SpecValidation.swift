e4acc8d87cb4ac22b23d014c32a6a5f9d8dac95c
yonaskolb <yonaskolb@gmail.com>
2019-08-13 23:11:04 +1000

    add package validation

--- Sources/ProjectSpec/SpecValidation.swift
+++ Sources/ProjectSpec/SpecValidation.swift
@@ -155,6 +155,10 @@ extension Project {
                             errors.append(.invalidSDKDependency(target: target.name, dependency: dependency.reference))
                         }
                     }
+                case .package:
+                    if packages[dependency.reference] == nil {
+                        errors.append(.invalidSwiftPackage(name: dependency.reference, target: target.name))
+                    }
                 default: break
                 }
             }
